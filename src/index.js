5b8afaf1341d5fc925f02d552c13659d944f2ccc
Natalia Romashko <sharkellgm@gmail>
2021-02-25 22:12:46 +0200

    feat: solve tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  return array.sort((a, b) => a - b)[0];
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  return array.sort((a, b) => b - a)[0];
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  const num = array.reduce((sum = 0, elem) => sum + elem);
+  return num / array.length;
 }
